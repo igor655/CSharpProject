088b054ef5d238fccfb57695e9abba484e34cc0d
igor655 <belyyigor@gmail.com>
2022-06-22 16:12:52 +0300

    Add project files.

---
+++ CSharpProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Using enum datatype
+// https://metanit.com/sharp/tutorial/2.12.php
+
+namespace CSharpProject
+{
+    internal class Program
+    {
+        enum DayTime
+        {
+            Morning,
+            Afternoon,
+            Evening,
+            Night
+        }
+
+        static void Main(string[] args)
+        {
+           const  DayTime now = DayTime.Evening;
+
+            Console.WriteLine(now);
+        }
+    }
+}
